71338f35a98108c091e38ac60dd4d7ac74fa8aa9
Xmader <xmader@outlook.com>
2020-01-13 04:04:31 -0500

    build tool

--- tool/build.js
+++ tool/build.js
@@ -1,40 +1,77 @@
 // @ts-check
 
-const { execSync, execFileSync } = require("child_process")
+const { exec, execFile } = require("child_process")
 const { findMSBuild } = require("./find-vs2017")
 
 /**
  * @param {string} cmd 
+ * @param {import("child_process").ExecOptions} options
  */
-const exec = (cmd) => {
-    return execSync(cmd, { encoding: "utf8" })
+const execAsync = (cmd, options = {}) => {
+    return new Promise((resolve, reject) => {
+        const c = exec(cmd, { encoding: "utf8", ...options })
+        c.stdin.end()
+        c.stdout.pipe(process.stdout)
+        c.stderr.pipe(process.stderr)
+        c.on("exit", (code) => {
+            if (code === 0) {
+                resolve(code)
+            } else {
+                reject(code)
+            }
+        })
+    })
+}
+
+
+/**
+ * @param {string} file
+ * @param {ReadonlyArray<string>} args
+ * @param {import("child_process").ExecFileOptions} options
+ */
+const execFileAsync = (file, args, options = {}) => {
+    return new Promise((resolve, reject) => {
+        const c = execFile(file, args, { encoding: "utf8", ...options })
+        c.stdin.end()
+        c.stdout.pipe(process.stdout)
+        c.stderr.pipe(process.stderr)
+        c.on("exit", (code) => {
+            if (code === 0) {
+                resolve(code)
+            } else {
+                reject(code)
+            }
+        })
+    })
 }
 
 const build = async () => {
-    exec("git submodule init")
-    exec("git submodule update")
-    exec("git apply deps/picotlsvs_sln.patch")
+    await execAsync("git submodule init")
+    await execAsync("git submodule update")
 
     if (process.platform == "win32") {
+        await execAsync("git apply deps/picotlsvs_sln.patch")
         const MSBuild = await findMSBuild()
-        execFileSync(MSBuild, [
+        await execFileAsync(MSBuild, [
             ".\\deps\\picotls\\picotlsvs\\picotlsvs.sln",
             "/nologo",
             "/p:Configuration=Release;Platform=x64"
-        ], { encoding: "utf8" })
+        ])
     } else {
-        exec("cmake .")
-        exec("make")
-        exec("make check")
+        await execAsync("cmake .", { cwd: "deps/picotls" })
+        await execAsync("make", { cwd: "deps/picotls" })
+        // await execAsync("make check", { cwd: "deps/picotls" })
     }
 
-    exec("git apply deps/picotls.patch")
+    await execAsync("git apply deps/picotls.patch")
 
-    exec("npm i")
+    await execAsync("node-gyp rebuild")
 
     // clean up
-    exec("git apply deps/picotls.patch --reverse")
-    exec("git apply deps/picotlsvs_sln.patch --reverse")
+    await execAsync("git apply deps/picotls.patch --reverse")
+    if (process.platform == "win32") {
+        await execAsync("git apply deps/picotlsvs_sln.patch --reverse")
+    }
 }
 
 build()
